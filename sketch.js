baad3a4fe09daf9bb872f55ef65b172f2872e855
Deetya222 <85564712+Deetya222@users.noreply.github.com>
2021-06-29 19:38:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
 
 function preload(){
-
+  seaImg=loadImage("sea.png")
+  shipImg1=loadImage("ship-1.png")
+  shipImg2=loadImage("ship-2.png")
+  shipImg3=loadImage("ship-3.png")
+  shipImg4=loadImage("ship-4.png")
+  ship.loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
 }
 
 function setup(){
   createCanvas(400,400);
-  
+  seaImg.addImage(seaImg)
+  ship1.addImage(ship1Img)
+  ship1.addAnimation("ship-1")
+
 }
 
 function draw() {
   background("blue");
- 
+  if (sea.x<0){
+    sea.x=sea.width/2
+
+  }
 }
